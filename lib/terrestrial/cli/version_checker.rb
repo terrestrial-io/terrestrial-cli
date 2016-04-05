313e77c6590d9bec00502dae2a0bb15cc64d4566
Niklas Begley <niklas.begley@gmail.com>
2016-04-05 15:27:15 +0100

    Better error handling for the version checker
    Don't want to halt the world if the version checker fails,
    so now cathing common problems

--- lib/terrestrial/cli/version_checker.rb
+++ lib/terrestrial/cli/version_checker.rb
@@ -11,13 +11,25 @@ module Terrestrial
         json = JSON.load(response.body)
 
         # Ignore the "v" in "v1.1.1"
-        version = json["tag_name"][1..-1]
+        begin 
+          version = json["tag_name"][1..-1]
+        rescue NoMethodError => e
+          # Github ratelimiting will change the JSON response.
+          # Keep calm and carry on.
+
+          version = Terrestrial::Cli::VERSION
+        end
 
         if version != Terrestrial::Cli::VERSION
           puts "There is an update for Terrestrial: #{version} (your version: #{Terrestrial::Cli::VERSION})"
           puts "Run 'gem update terrestrial-cli' to update."
           puts ""
         end
+      rescue JSON::ParserError => e
+        # Don't worry about JSON parsing errors - just carry on
+      rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
+               Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
+        # Don't worry about Net HTTP errors - just carry on
       end
     end
   end
