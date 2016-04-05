04b1c8d3f3c08a3778fce686525103ba5f2be45f
Niklas Begley <niklas.begley@gmail.com>
2016-04-05 18:42:16 +0100

    Test was populating global namespace and clashing

--- spec/terrestrial/launch_args_builder_spec.rb
+++ spec/terrestrial/launch_args_builder_spec.rb
@@ -2,17 +2,17 @@ require 'spec_helper'
 
 describe Terrestrial::Cli::SimulatorLauncher::LaunchArgsBuilder do
 
-  Builder = Terrestrial::Cli::SimulatorLauncher::LaunchArgsBuilder
+  LaunchArgsBuilder = Terrestrial::Cli::SimulatorLauncher::LaunchArgsBuilder
 
   it 'joins arguments together into a string' do
-    expect(Builder.build({
+    expect(LaunchArgsBuilder.build({
       'Foo' => 'bar',
       'Fizz' => 'Buz'
     })).to eq '-Foo "bar" -Fizz "Buz"'
   end
 
   it 'does converts booleans to unescaped YES and NO' do
-    expect(Builder.build({
+    expect(LaunchArgsBuilder.build({
       'Foo' => 'bar',
       'Fizz' => true,
       'Chuck' => false,
